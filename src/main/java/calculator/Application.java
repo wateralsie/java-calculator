e4645af84fceab32a8a22d048376b7402784dc2b
wateralsie <minee1913@gmail.com>
2025-10-15 15:39:12 +0900

    feat: 기본 구분자 바탕으로 숫자 추출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app = new Application();
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+    }
+
+    public StringTokenizer parseNumbers(String input) {
+        return new StringTokenizer(input, ",:");
     }
 }
