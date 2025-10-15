650a6628da301027a0884f9b490eae3ece74062e
wateralsie <minee1913@gmail.com>
2025-10-15 17:54:28 +0900

    feat: 커스텀 구분자 추출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,8 @@ package calculator;
 
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,8 +11,9 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
-        int result = app.addNumbers(app.parseNumbers(userInput));
-        System.out.println("결과 : " + result);
+        app.addCustomDelimiter(userInput.replace("\\n", "\n"));
+//        int result = app.addNumbers(app.parseNumbers(userInput));
+//        System.out.println("결과 : " + result);
     }
 
     public StringTokenizer parseNumbers(String input) {
@@ -23,5 +26,13 @@ public class Application {
             total += Integer.parseInt(st.nextToken());
         }
         return total;
+    }
+
+    public void addCustomDelimiter(String input) {
+        Pattern customDelimiterPattern = Pattern.compile("//(.)\\R");
+        Matcher matcher = customDelimiterPattern.matcher(input);
+        while (matcher.find()) {
+            System.out.println(matcher.group(1));
+        }
     }
 }
