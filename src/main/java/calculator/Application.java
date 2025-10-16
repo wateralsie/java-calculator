a99d388ae034daa22af7ee879573edb77c1cfed4
wateralsie <minee1913@gmail.com>
2025-10-16 14:38:28 +0900

    refactor: 입력 클래스 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,9 +8,8 @@ import java.util.regex.Pattern;
 public class Application {
     public static void main(String[] args) {
         Application app = new Application();
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine().replace("\\n", "\n");
+        Input input = new Input();
+        String userInput = input.read();
 
         String customDelims = app.addCustomDelimiter(userInput.replace("\\n", "\n"));
         String[] dividedUserInput = userInput.split("\\R");
---
+++ src/main/java/calculator/Input.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Input {
+
+    private static final String PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+    private final Scanner scanner;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String read() {
+        System.out.println(PROMPT_MESSAGE);
+        String input = scanner.nextLine();
+        return convertToLineBreak(input);
+    }
+
+    // todo: string util로 변환 가능해보임
+    private String convertToLineBreak(String str) {
+        return str.replace("\\n", "\n");
+    }
+}
