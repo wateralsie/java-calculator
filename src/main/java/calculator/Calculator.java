1b27ab1e4b54b0662096be0047942575dd239894
wateralsie <minee1913@gmail.com>
2025-10-17 21:02:47 +0900

    feat: 엣지 케이스 대응 로직 추가 및 예외 처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,9 +8,20 @@ public class Calculator {
 
     public void run() {
         String userInput = new Input().read();
-        Extractor extractor = new Extractor(userInput);
-        StringTokenizer numberList = extractor.parseNumbers();
-        int result = add(numberList);
+        int result;
+        // 빈 값이거나 공백으로만 구성
+        if (userInput == null || userInput.isBlank()) result = 0;
+        // 숫자만 존재
+        else if (userInput.matches("[0-9]+")) result = Integer.parseInt(userInput);
+        // 유효한 구분자만 존재
+        else if (userInput.matches("[,:]+")) result = 0;
+        else {
+            Extractor extractor = new Extractor(userInput);
+            StringTokenizer numberList = extractor.parseNumbers();
+            // 커스텀 구분자만 있고 숫자가 없음 (커스텀 구분자 지정 양식 + 커스텀 구분자만)
+            if (numberList.countTokens() == 0) result = 0;
+            else result = add(numberList);
+        }
         System.out.println("결과 : " + result);
     }
 
--- src/main/java/calculator/Extractor.java
+++ src/main/java/calculator/Extractor.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 public class Extractor {
 
     private static final String DEFAULT_DELIMS = ",:";
+    // 커스텀 구분자 글자 길이 2 이상
     private static final Pattern CUSTOM_DELIM_PATTERN = Pattern.compile("//(.)\\R");
     private String delims;
     private String numbersString;
@@ -16,12 +17,21 @@ public class Extractor {
     }
 
     public StringTokenizer parseNumbers() {
-        return new StringTokenizer(numbersString, delims);
+        StringTokenizer tokens = new StringTokenizer(numbersString, delims);
+        while (tokens.hasMoreTokens()) {
+            // 추출한 결과가 숫자가 아닐 때
+            if (!tokens.nextToken().matches("[0-9]+")) throw new IllegalArgumentException("에러 3");
+        }
+        return tokens;
     }
 
     private void parse(String str) {
         int lastLineBreak = str.lastIndexOf("\n") + 1;
+        // 원래라면 커스텀 구분자가 없어야함
         if (lastLineBreak == 0) {
+            // 숫자와 기본 구분자로만 이루어져 있지 않으면
+            // 유효하지 않은 구분자 존재 or 커스텀 구분자 지정 양식 잘못됨 or 유효하지 않은 구분자만 있을 때
+            if (!str.matches("[0-9,:]+")) throw new IllegalArgumentException("에러 1");
             delims = DEFAULT_DELIMS;
             numbersString = str;
         } else {
@@ -30,9 +40,13 @@ public class Extractor {
         }
     }
 
-    private String extractCustomDelimiter(String input) {
+    private String extractCustomDelimiter(String str) {
+        // 커스텀 구분자 문자열 뒷부분에 지정
+        // 커스텀 구분자가 숫자, -
+        if (Pattern.compile("[0-9-]").matcher(str).find()) throw new IllegalArgumentException("에러 2");
+
         StringBuilder sb = new StringBuilder();
-        Matcher matcher = CUSTOM_DELIM_PATTERN.matcher(input);
+        Matcher matcher = CUSTOM_DELIM_PATTERN.matcher(str);
         while (matcher.find()) {
             sb.append(matcher.group(1));
         }
